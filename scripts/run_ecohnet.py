ecc609987f460b40fc7c588dbd9fc56b664fa2c3
kecosz <60416241+kecosz@users.noreply.github.com>
2023-09-28 10:34:08 +0900

    Add files via upload

--- scripts/run_ecohnet.py
+++ scripts/run_ecohnet.py
@@ -12,28 +12,22 @@ from ecohnet.utils.preprocess import std
 REP = 10000
 OUT_DIR = Path("out")
 DATA_DIR = Path("data")
+DATA_FILE = "rdata_011322_2.csv"
 DATE = datetime.date.today().strftime("%Y%m%d")
-OUT_FILE_NAME = f"kasmircall_{REP}_{DATE}.dill"
+OUT_FILE_NAME = f"{DATA_FILE}_{REP}_{DATE}.dill"
 
 if __name__ == "__main__":
-    data_path = DATA_DIR / "rdata_011322_2.csv"
+    data_path = DATA_DIR / DATA_FILE
 
     assert data_path.exists()
     assert data_path.is_file()
 
     rdata = pd.read_csv(data_path, header=0)
     print(rdata.head(4))
-    rdata = rdata.rename(
-        columns={
-            "Anabena": "Nostocales",
-            "Planktothrix": "Oscillatoriales",
-            "Actinocyclus": "Thalassiosiraceae",
-        }
-    )
     pprint.pprint([f"{i} {col}" for i, col in enumerate(rdata.columns)])
 
-    datk0 = rdata.iloc[137:, 3:]
-    datk0.head(4)
+    datk0 = rdata
+    print(datk0.head(4))
 
     datk = datk0.to_numpy() ** 0.5
 
@@ -43,9 +37,9 @@ if __name__ == "__main__":
 
     tu = time.time()
     rcc, rcprd, rcmat, rw = RCall(std(datk), (0.95, 0.95, 0.001, 8), seed=42, rep=REP)
-    kasmircall = (rcc, rcprd, rcmat, rw, datk)
+    rcall = (rcc, rcprd, rcmat, rw, datk)
 
     print(f"elapsed time of RCall: {time.time() - tu}")
 
     with open(out_fpath, "wb") as f:
-        dill.dump(kasmircall, f)
+        dill.dump(rcall, f)
