de5008e72044e6e7bce995fc1e63edcdb78cc623
kecosz <60416241+kecosz@users.noreply.github.com>
2024-06-12 18:43:02 +0900

    Add files via upload

--- src/ecohnet/utils/reservoir.py
+++ src/ecohnet/utils/reservoir.py
@@ -248,7 +248,7 @@ def comp(
             pn = ilambda * (pn - np.outer(gn, xi) * pn)
             # state update
             xi = l1
-            wou = wou + gn @ vn.reshape(1, -1)
+            wou = wou + vn * gn
     return yprds
 
 
@@ -312,7 +312,7 @@ def compd(
             pn = ilambda * (pn - np.outer(gn, xi) * pn)
             # state update
             xi = l1
-            wou = wou + gn @ vn.reshape(1, -1)
+            wou = wou + vn * gn
     return yprds
 
 
@@ -362,5 +362,5 @@ def nu_compd(
             pn = ilambda * (pn - np.outer(gn, xi) * pn)
             # state update
             xi = l1
-            wou = wou + gn @ vn.reshape(1, -1)
+            wou = wou + vn * gn
     return yprds
