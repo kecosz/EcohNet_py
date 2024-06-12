b46d69b2f8e2b1f39af999055f3315e3523e6b72
kecosz <60416241+kecosz@users.noreply.github.com>
2024-06-12 15:08:50 +0900

    Add files via upload

--- src/ecohnet/reservoir.py
+++ src/ecohnet/reservoir.py
@@ -242,13 +242,12 @@ def comp(
         yprds[i] = wou @ l1
         for _ in range(n_update):
             ouprd = wou @ l1
-            vn = output - ouprd
+            vn = output[i] - ouprd
             tmp = pn @ xi
-            gn = (ilambda * tmp / (1 + ilambda * (xi @ tmp))).reshape(-1, 1)
-            pn = ilambda * (pn - (gn @ xi.reshape(1, -1)) * pn)
+            gn = ilambda * tmp / (1 + ilambda * (xi @ tmp))
+            pn = ilambda * (pn - (gn @ xi) * pn)
             # state update
             xi = l1
-            wou = wou + gn @ vn.reshape(1, -1)
             wou = wou + gn * vn
     return yprds
 
@@ -307,13 +306,13 @@ def compd(
         yprds[i] = wou @ l1
         for _ in range(n_update):
             ouprd = wou @ l1
-            vn = output - ouprd
+            vn = oux - ouprd
+            # gn = ilambda * (pn @ xi) / (1 + ilambda * (xi @ pn) @ xi)
             tmp = pn @ xi
-            gn = (ilambda * tmp / (1 + ilambda * (xi @ tmp))).reshape(-1, 1)
-            pn = ilambda * (pn - (gn @ xi.reshape(1, -1)) * pn)
+            gn = ilambda * tmp / (1 + ilambda * (xi @ tmp))
+            pn = ilambda * (pn - (gn @ xi) * pn)
             # state update
             xi = l1
-            wou = wou + gn @ vn.reshape(1, -1)
             wou = wou + gn * vn
     return yprds
 
@@ -357,13 +356,13 @@ def nu_compd(
         l1 = np.tanh(win @ (input[i] * dropout[i]) + ww @ xi)
         yprds[i] = wou @ l1
         for _ in range(n_update):
-            ouprd = wou @ l1
-            vn = output - ouprd
+            ouprd = np.dot(wou, l1)
+            vn = output[i] - ouprd
+            # gn = ilambda * (pn @ xi) / (1 + ilambda * (xi @ pn) @ xi)
             tmp = pn @ xi
-            gn = (ilambda * tmp / (1 + ilambda * (xi @ tmp))).reshape(-1, 1)
-            pn = ilambda * (pn - (gn @ xi.reshape(1, -1)) * pn)
+            gn = ilambda * tmp / (1 + ilambda * (xi @ tmp))
+            pn = ilambda * (pn - (gn @ xi) * pn)
             # state update
             xi = l1
-            wou = wou + gn @ vn.reshape(1, -1)
             wou = wou + gn * vn
     return yprds
